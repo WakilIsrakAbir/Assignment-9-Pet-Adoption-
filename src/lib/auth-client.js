611e47c583070b141569ea35204e9754f1405a34
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-08-16 21:08:37 +0600

    added dynamic urls for vercel deployment

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000/api/auth" 
+    baseURL: process.env.NEXT_PUBLIC_APP_URL ? `${process.env.NEXT_PUBLIC_APP_URL}/api/auth` : "http://localhost:3000/api/auth" 
 })
